f1f264fdb764b1dd03050ab97147907130c9bb61
Karam Nasser <47743218+karamnaser123@users.noreply.github.com>
2024-07-01 13:05:10 +0300

    update

--- api/middleware.py
+++ api/middleware.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponseForbidden
+from django.http import HttpResponseForbidden, HttpResponseRedirect
 from django.utils.deprecation import MiddlewareMixin
+from django.urls import reverse
 
 class AdminOnlyMiddleware(MiddlewareMixin):
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if request.path.startswith('/api/') and not request.user.is_staff:
-            return HttpResponseForbidden("You do not have permission to access this page.")
+        if request.path.startswith('/api/'):
+            if not request.user.is_authenticated:
+                return HttpResponseRedirect(reverse('login'))
+            elif not request.user.is_staff:
+                return HttpResponseForbidden("You do not have permission to access this page.")
         return None
